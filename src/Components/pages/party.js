dfbcaf3047747039ada28ff9b4545f2354f4591b
Jliv316 <jclivingston316@gmail.com>
2018-09-03 13:10:07 -0600

    build

--- src/Components/pages/party.js
+++ src/Components/pages/party.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import Playlist from '../playlistComponent/Playlist';
 import PartyService from '../partyComponent/partyService';
-import PartyButton from '../partyComponent/partyButton';
-import Button from 'react-toolbox/lib/button/Button';
 import { getQueryParams } from '../../utils';
-import axios from 'axios';
 
 class Party extends Component {
   constructor(props) {
@@ -16,23 +13,7 @@ class Party extends Component {
      }
   }
 
-  handleClick = () => {
-    axios.post('http://localhost:3166/api/v1/party', {
-      client_token: this.props.token
-    })
-      .then(response => {
-        console.log(response)
-        this.setState({ tracks: response.data.tracks })
-      })
-      .catch(error => console.log(error))
-  }
-
   render() {
-    const tracks = this.state.tracks.map((track) => (
-      <div>
-        <h1>{track.name}</h1>
-      </div>
-    ));
     return (
       <div className="homeContainer">
         <div className="pimg1">
@@ -45,12 +26,7 @@ class Party extends Component {
 
         <section className="section section-light">
           <p>
-            <Button onClick={this.handleClick}>
-              Start a Party!
-             </Button>
-            <div id="layout-content" className="layout-content-wrapper">
-              <div className="panel-list">{tracks}</div>
-            </div>
+            <PartyService tracks={this.state.tracks} />
           </p>
         </section>
 
--- src/Components/partyComponent/partyService.js
+++ src/Components/partyComponent/partyService.js
@@ -4,19 +4,19 @@ import axios from 'axios';
 class PartyService extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = { tracks: [] };
+    this.state = { tracks: [] };
   }
 
-  // componentDidMount() {
-  //   axios.post('http://localhost:3166/api/v1/party', {
-  //     client_token: this.props.token
-  //   })
-  //     .then(response => {
-  //       console.log(response)
-  //       this.setState({ tracks: response.data.tracks })
-  //     })
-  //     .catch(error => console.log(error))
-  // }
+  componentDidMount() {
+    axios.post('http://localhost:3166/api/v1/party', {
+      client_token: this.props.token
+    })
+      .then(response => {
+        console.log(response)
+        this.setState({ tracks: response.data.tracks })
+      })
+      .catch(error => console.log(error))
+  }
 
   render() {
     const tracks = this.props.tracks.map((track) => (
